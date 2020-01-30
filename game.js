70c933461a064a60368c19118d5be8951c489638
hunterwaters <55896961+hunterwaters@users.noreply.github.com>
2020-01-29 17:14:12 -0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,53 @@
+
+const STORE = [
+    {
+        question: "Which of the following is a red wine?", 
+        answers: [
+            "Chardonnay",
+            "Merlot",
+            "Riesling",
+            "Sauvignon Blanc",
+        ],
+         correctAnswer: 'Merlot'
+    },
+    {
+        question: "Where is the Malbec grape primarily grown?",
+         answers: [
+             "United States",
+             "France",
+             "Argentina",
+             "Italy",
+         ],
+        correctAnswer: 'Argentina'
+    },
+    {
+        question: "What grape is the best pairing with steak?",
+        answers: [
+             "Pinot Noir",
+            "Cabernet Sauvignon",    
+             "Chardonnay",
+             "Moscato",
+        ],
+         correctAnswer: 'Cabernet Sauvignon'
+    },
+   {
+       question: "Where is the most wine produced in the United States?",
+       answers: [
+            "Oregon",
+            "Washington",
+            "California",
+            "Moscato",
+       ],
+       correctAnswer: 'California'
+   },
+   {
+       question: "Which of the following is a white wine?",
+       answers: [
+            "Cabernet Franc",
+            "Petit Verdot",
+            "Nebbiolo",
+            "Chenin Blanc",
+       ],
+       correctAnswer: 'Chenin Blanc'
+   }
+];
